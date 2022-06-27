9ae6589bd6f992a89d1b1c231506bf8f3e72114e
mengiefen <mengiefen@gmail.com>
2022-06-27 23:29:27 +0300

    add a user_avatar helper function

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def user_avatar(user, size = 30)
+    if user.avatar.attached?
+      user.avatar.variant(resize: "#{size}x#{size}!")
+    else
+      user.avatar.attach_avatar = 'defualt-avatar.png'
+    end
+  end
 end
