e77633f0f48e2d0b64042f2bef46d724f7c246fe
mengiefen <mengiefen@gmail.com>
2022-06-27 16:33:53 +0300

    change form styles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :update_allowed_parameters, if: :devise_controller?
+  before_action :authenticate_user!, except: [:home]
+
+
+  private
+  def after_sign_out_path_for(resource_or_scope)
+    home_path
+  end
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name) }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: "users#index"
+
+  resources :users
+
+
+  get '/home',to: 'homes#home'
+
 end
