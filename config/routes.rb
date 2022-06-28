ea6035a374fd31b7968bb3c78cbbbd76249939a4
mengiefen <mengiefen@gmail.com>
2022-06-28 09:31:53 +0300

    add routes to the categories page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root to: "users#index"
+  root to: "utilities#index"
 
   resources :users
 
 
   get '/home',to: 'homes#home'
+  get '/utilities', to: 'utilities#index', as: 'categories'
 
 end
