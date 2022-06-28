2628b0c2c6aa100b8649e566be7f15e0b2feca64
mengiefen <mengiefen@gmail.com>
2022-06-28 20:37:41 +0300

    change configuration in developement.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,6 +2,8 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  # config.active_storage.variant_processor = :mini_magick
+  # config.active_storage.variant_processor = :vips  # Default is vips
 
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
