1586ac7d15a21b2aefb9de9fcb84cf77c3796a9d
mengiefen <mengiefen@gmail.com>
2022-06-28 09:31:11 +0300

    add validation for payment and utility table

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -2,4 +2,6 @@ class Payment < ApplicationRecord
   belongs_to :user
   has_many :utility_payments
   has_many :utilities, through: :utility_payments
+  validates :name, presence: true
+  validates :amount, numericality: true, default: 0
 end
