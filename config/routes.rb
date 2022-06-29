72b97b1245aaec1b875229a3233ac67577c7a0dd
mengiefen <mengiefen@gmail.com>
2022-06-29 22:08:29 +0300

    remove confirmable from devise setting

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root to: "utilities#index"
+  root to: "homes#home"
 
   resources :users do 
     resources :utilities, only: [:index, :new, :create, :show]
