c01949fa0e3746f16ea3628d67844576a3283375
mengiefen <mengiefen@gmail.com>
2022-06-29 17:00:57 +0300

    add js code for mobile navbar

--- app/javascript/navbar.js
+++ app/javascript/navbar.js
@@ -1,15 +1,17 @@
 //Navbar Code 
-
 const menuOpen = document.querySelector('.btn-open')
 const navbar = document.querySelector(".navbar");
 const menuClose = document.querySelector(".btn-close");
+const body = document.querySelector('body');
 
 menuOpen.addEventListener('click', ()=> {
   navbar.style.display = "flex";
+  body.classList.add('blur');
 })
 
 menuClose.addEventListener('click', ()=> {
   navbar.style.display = "none";
+  body.classList.remove('blur');
 })
 
 // menuItems.forEach((element) => {
