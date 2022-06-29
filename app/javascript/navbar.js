cf7ef4fae44cbf7cd2f1a5386aa3078cb9215bf4
mengiefen <mengiefen@gmail.com>
2022-06-29 22:45:30 +0300

    fix bug with javascript

--- app/javascript/navbar.js
+++ app/javascript/navbar.js
@@ -1,19 +1,25 @@
 //Navbar Code 
-const menuOpen = document.querySelector('.btn-open')
-const navbar = document.querySelector(".navbar");
-const menuClose = document.querySelector(".btn-close");
-const body = document.querySelector('body');
+document.addEventListener("DOMContentLoaded", function(event) {
+  
+  const menuOpen = document.querySelector('.btn-open')
+  const navbar = document.querySelector(".navbar");
+  const menuClose = document.querySelector(".btn-close");
+  const body = document.querySelector('body');
 
-menuOpen.addEventListener('click', ()=> {
-  navbar.style.display = "flex";
-  body.classList.add('blur');
-})
+  menuOpen.addEventListener('click', ()=> {
+    navbar.style.display = "flex";
+    body.classList.add('blur');
+  })
 
-menuClose.addEventListener('click', ()=> {
-  navbar.style.display = "none";
-  body.classList.remove('blur');
-})
+  menuClose.addEventListener('click', ()=> {
+    navbar.style.display = "none";
+    body.classList.remove('blur');
+  })
+});
 
-// menuItems.forEach((element) => {
-//   element.addEventListener('click', collapseMenu);
-// });
+
+
+
+
+
+
