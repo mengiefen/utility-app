3e28b495e89bd495dedcb34203537274a075063d
mengiefen <mengiefen@gmail.com>
2022-06-30 15:32:10 +0300

    include test for  utility model

--- spec/models/utility_spec.rb
+++ spec/models/utility_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Utility, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @utility = create(:utility)
+  end
+
+  describe 'validates' do
+    it 'utility name to be Utility' do
+      expect(@utility.name).to eq 'Utility'
+    end
+
+    it 'Number of utilities must be an integer it to be 1.' do
+      expect(Utility.all.count).to eq 1
+    end
+
+    it 'utility user name should be John.' do
+      expect(@utility.user.name).to eq 'John'
+    end
+  end
 end
