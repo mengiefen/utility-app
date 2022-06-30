82b8b33dffc01ca0d8c146bc6c49a294350c25aa
mengiefen <mengiefen@gmail.com>
2022-06-30 15:31:48 +0300

    add rspec test for utility_payment model

--- spec/models/utility_payment_spec.rb
+++ spec/models/utility_payment_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe UtilityPayment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @utility_payment = FactoryBot.create(:utility_payment)
+  end
+
+  describe 'validates' do
+    it 'utitlity payment name to be Electricity' do
+      expect(@utility_payment.payment.name).to eq 'Random Payment'
+    end
+
+    it 'Number of utility payments must be an integer it to be 1.' do
+      expect(UtilityPayment.all.count).to eq 1
+    end
+
+    it 'utility payments category name should be "Utility".' do
+      expect(@utility_payment.utility.name).to eq 'Random Utility'
+    end
+  end
 end
