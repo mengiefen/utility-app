82f6cc7391a8647c9a836c40875d9c8f181888dc
mengiefen <mengiefen@gmail.com>
2022-07-01 19:41:22 +0300

    fix style linter error

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,17 @@
 module ApplicationHelper
-  def user_avatar(user, _size = 30)
+  def user_avatar(user, size = 30)
     if user.avatar.attached?
-      user.avatar.url
+      user.avatar.representation(resize_to_limit: [size, size])
+      # user.avatar.url
     else
       'defualt-avatar.png'
     end
   end
 
-  def utility_icon(utility, _size)
+  def utility_icon(utility, size)
     if utility.icon.attached?
-      utility.icon.url
+      utility.icon.representation(resize_to_limit: [size, size])
+      # utility.icon.url
     else
       utility.icon.attach_icon = 'default_icon.png'
     end
