f06117e89559e5f3b2bb87a69ffd766cfcfbd511
mengiefen <mengiefen@gmail.com>
2022-07-01 17:02:00 +0300

    remove icon column

--- db/migrate/20220627201155_create_utilities.rb
+++ db/migrate/20220627201155_create_utilities.rb
@@ -1,8 +1,7 @@
 class CreateUtilities < ActiveRecord::Migration[7.0]
   def change
     create_table :utilities do |t|
-      t.string :name
-      t.string :icon
+      t.string :name 
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
