3b88792ce8b464496c584a5e3cf0b8f404b90cef
mengiefen <mengiefen@gmail.com>
2022-07-01 19:32:03 +0300

    modify the helper functions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,15 @@
 module ApplicationHelper
-  def user_avatar(user, size = 30)
+  def user_avatar(user, _size = 30)
     if user.avatar.attached?
-      user.avatar.representation(resize_to_limit: [size, size])
+      user.avatar.url
     else
       'defualt-avatar.png'
     end
   end
 
-  def utility_icon(utility, size)
+  def utility_icon(utility, _size)
     if utility.icon.attached?
-      utility.icon.representation(resize_to_limit: [size, size])
+      utility.icon.url
     else
       utility.icon.attach_icon = 'default_icon.png'
     end
